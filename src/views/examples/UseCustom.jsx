00feb6480367361e5ae9202f98bdb53227493069
heberGustavo <heber.gbarbosa@hotmail.com>
2024-10-19 11:09:44 -0300

    Example 2 with UseCustom

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -2,29 +2,46 @@ import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 import { useCounter } from '../../hooks/useCounter'
-
-const UseCustom = (props) => {
-    
-    //const [count, , dec] = useCounter();
-    const [count, inc, dec] = useCounter();
-
+import { useFetch } from '../../hooks/useFetch'
+ 
+const UseRef = props => {
+ 
+    const [count, inc, dec] = useCounter()
+ 
+    const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+    const response = useFetch(url)
+ 
+    function showList (data) {
+        return data.map(item => (
+            <li key={item.nome}> {item.nome} - {item.sigla}</li>
+        ))
+    } 
+ 
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
-                subtitle="Vamos aprender como criar o nosso próprio Hook!"
-            />
-
-            <SectionTitle title="Exercicio 01" />
+                subtitle="Vamos aprender como criar o nosso próprio Hook!" />
+ 
+            <SectionTitle title="Exercício #01" />
             <div className="center">
                 <span className="text">{count}</span>
                 <div>
-                    <buttuon className="btn" onClick={() => dec()} >-1</buttuon>
-                    <buttuon className="btn" onClick={() => inc()} >+1</buttuon>
+                    <button className="btn"
+                        onClick={() => dec()}>-1</button>
+                    <button className="btn"
+                        onClick={() => inc()}>+1</button>
                 </div>
+            </div>
+ 
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <ul>
+                    {!response.loading ? showList(response.data) : false}
+                </ul>
             </div>
         </div>
     )
 }
-
-export default UseCustom
+ 
+export default UseRef
